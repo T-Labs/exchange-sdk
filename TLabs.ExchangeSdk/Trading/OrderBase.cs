7f2ed0ca7619d5a1ed74fd5b542ce4e78a369f98
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2021-06-17 00:32:24 +0600

    add Required, clean up

--- TLabs.ExchangeSdk/Trading/OrderBase.cs
+++ TLabs.ExchangeSdk/Trading/OrderBase.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TLabs.ExchangeSdk.Trading
 {
@@ -16,13 +17,16 @@ namespace TLabs.ExchangeSdk.Trading
             Amount = amount;
         }
 
+        [Required]
         public string CurrencyPairCode { get; set; }
+
         public bool IsBid { get; set; }
         public decimal Amount { get; set; }
         public decimal Price { get; set; }
         public DateTimeOffset DateCreated { get; set; } = DateTimeOffset.UtcNow;
 
         public bool HasSameOrderbookSide(Order order2) => this.IsBid == order2.IsBid;
+
         public bool HasSameCurrencyPair(Order order2) => this.CurrencyPairCode == order2.CurrencyPairCode;
 
         public override string ToString() => $"{nameof(OrderBase)}({(IsBid ? "bid" : "ask")} amount:{Amount} price:{Price})";
