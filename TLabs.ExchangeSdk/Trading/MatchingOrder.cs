41ac0dca9f9b7ddf42141217f11bdd28150658d7
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2021-06-18 18:35:13 +0600

    add MatchingOrder

---
+++ TLabs.ExchangeSdk/Trading/MatchingOrder.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLabs.ExchangeSdk.Trading
+{
+    public class MatchingOrder : Order
+    {
+        public MatchingOrder()
+        {
+        }
+
+        public MatchingOrder(bool isBid, string currencyPairCode, decimal price, decimal amount)
+        {
+            IsBid = isBid;
+            CurrencyPairCode = currencyPairCode;
+            Price = price;
+            Amount = amount;
+        }
+
+        /// <summary>How many times was this order sent to other exchange</summary>
+        public int LiquidityBlocksCount { get; set; }
+
+        public virtual List<MatchingDeal> DealList { get; set; }
+
+        public MatchingOrder Clone() => (MatchingOrder)MemberwiseClone();
+    }
+}
