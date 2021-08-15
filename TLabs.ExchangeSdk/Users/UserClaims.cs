a7421c45dc51d170be47e2778149548605867c48
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2021-08-16 01:34:31 +0600

    add UserClaims from Shared project

---
+++ TLabs.ExchangeSdk/Users/UserClaims.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLabs.ExchangeSdk.Users
+{
+    public class UserClaims
+    {
+        public const string CantChat = "CantChat";
+        public const string PolicyCanChat = "PolicyCanChat";
+        public const string CantTrade = "CantTrade";
+        public const string PolicyCanTrade = "PolicyCanTrade";
+        public const string CantRefill = "CantRefill";
+        public const string PolicyCanRefill = "PolicyCanRefill";
+        public const string CantWithdraw = "CantWithdraw";
+        public const string PolicyCanWithdraw = "PolicyCanWithdraw";
+    }
+}
---
+++ TLabs.ExchangeSdk/Users/UserRoles.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLabs.ExchangeSdk.Users
+{
+    public class UserRoles
+    {
+        public const string Admin = "Administrator";
+    }
+}
