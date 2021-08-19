6caca2192defa8179df9da7b05a9e666ac774a0c
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2021-08-19 22:27:46 +0600

    add CalculateFromEquivalent method

--- TLabs.ExchangeSdk/Trading/MarketdataHelper.cs
+++ TLabs.ExchangeSdk/Trading/MarketdataHelper.cs
@@ -10,11 +10,29 @@ namespace TLabs.ExchangeSdk.Trading
 {
     public static class MarketdataHelper
     {
+
         /// <summary>Calculate equivalent of amount</summary>
         /// <param name="quotesDict">preloaded dict with quotes</param>
         /// <param name="equivalentCurrency">BTC or USDT</param>
         public static decimal? CalculateEquivalent(this decimal amount, string currencyCode,
             Dictionary<string, Quote> quotesDict, string equivalentCurrency = "USDT")
+        {
+            var rate = GetQuoteRate(currencyCode, quotesDict, equivalentCurrency);
+            return rate == null ? null : (amount * rate.Value).RoundDown(CurrenciesCache.Digits);
+        }
+
+        /// <summary>Calculate amount in currencyCode from its equivalent in USDT or BTC</summary>
+        /// <param name="quotesDict">preloaded dict with quotes</param>
+        /// <param name="equivalentCurrency">BTC or USDT</param>
+        public static decimal? CalculateFromEquivalent(this decimal amount, string currencyCode,
+            Dictionary<string, Quote> quotesDict, string equivalentCurrency = "USDT")
+        {
+            var rate = GetQuoteRate(currencyCode, quotesDict, equivalentCurrency);
+            return rate == null ? null : (amount / rate.Value).RoundDown(CurrenciesCache.Digits);
+        }
+
+        public static decimal? GetQuoteRate(string currencyCode, Dictionary<string, Quote> quotesDict,
+            string equivalentCurrency = "USDT")
         {
             var quote = quotesDict.GetValueOrDefault(currencyCode, null);
             decimal? rate = equivalentCurrency switch
@@ -23,9 +41,7 @@ namespace TLabs.ExchangeSdk.Trading
                 "BTC" => quote?.BtcRate,
                 _ => null,
             };
-            if (rate == null)
-                return null;
-            return (amount * rate.Value).RoundDown(CurrenciesCache.Digits);
+            return rate;
         }
     }
 }
