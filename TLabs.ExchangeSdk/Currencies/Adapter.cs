e916d3534d43a02faca7d862dc93b1944a9ffeba
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2022-03-23 19:30:01 +0600

    update Adapter model

--- TLabs.ExchangeSdk/Currencies/Adapter.cs
+++ TLabs.ExchangeSdk/Currencies/Adapter.cs
@@ -20,21 +20,24 @@ namespace TLabs.ExchangeSdk.Currencies
 
         public List<CurrencyAdapter> CurrencyAdapters { get; set; }
 
+        public static Adapter AdapterBtc = new Adapter { Code = "btc", Name = "Bitcoin", MainCurrencyCode = "BTC", };
+        public static Adapter AdapterDash = new Adapter { Code = "dash", Name = "Dash", MainCurrencyCode = "DASH", };
+        public static Adapter AdapterDoge = new Adapter { Code = "doge", Name = "Dogecoin", MainCurrencyCode = "DOGE", };
+        public static Adapter AdapterLtc = new Adapter { Code = "ltc", Name = "Litecoin", MainCurrencyCode = "LTC", };
+        public static Adapter AdapterEth = new Adapter { Code = "eth", Name = "Ethereum", MainCurrencyCode = "ETH", };
+        public static Adapter AdapterBsc = new Adapter { Code = "bsc", Name = "Binance Smart Chain", MainCurrencyCode = "BNB", };
+        public static Adapter AdapterTrx = new Adapter { Code = "trx", Name = "Tron", MainCurrencyCode = "TRX", };
+        public static Adapter AdapterDel = new Adapter { Code = "del", Name = "Decimal", MainCurrencyCode = "DEL", };
+        public static Adapter AdapterPzm = new Adapter { Code = "pzm", Name = "Prizm", MainCurrencyCode = "PZM", };
+        public static Adapter AdapterAdvcash = new Adapter { Code = "advcash", Name = "AdvCash", MainCurrencyCode = null, IsFiat = true, };
+
         public static List<Adapter> DefaultAdapters = new()
         {
-            new Adapter { Code = "btc", Name = "Bitcoin", MainCurrencyCode = "BTC", },
-            new Adapter { Code = "dash", Name = "Dash", MainCurrencyCode = "DASH", },
-            new Adapter { Code = "doge", Name = "Dogecoin", MainCurrencyCode = "DOGE", },
-            new Adapter { Code = "ltc", Name = "Litecoin", MainCurrencyCode = "LTC", },
-            new Adapter { Code = "eth", Name = "Ethereum", MainCurrencyCode = "ETH", },
-            new Adapter { Code = "bsc", Name = "Binance Smart Chain", MainCurrencyCode = "BNB", },
-            new Adapter { Code = "trx", Name = "Tron", MainCurrencyCode = "TRX", },
-            new Adapter { Code = "del", Name = "Decimal", MainCurrencyCode = "DEL", },
-            new Adapter { Code = "pzm", Name = "Prizm", MainCurrencyCode = "PZM", },
-            new Adapter { Code = "advcash", Name = "AdvCash", MainCurrencyCode = null, IsFiat = true, },
+            AdapterBtc, AdapterDash, AdapterDoge, AdapterLtc,
+            AdapterEth, AdapterBsc, AdapterTrx, AdapterDel, AdapterPzm, AdapterAdvcash,
         };
 
         public override string ToString() =>
-            $"{nameof(Adapter)}({Code}, {Name}, MainCurrencyCode:{MainCurrencyCode})";
+                    $"{nameof(Adapter)}({Code}, {Name}, MainCurrencyCode:{MainCurrencyCode})";
     }
 }
