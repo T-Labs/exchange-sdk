7bbf5febcfc90eb55b3d52e2de1bdde57e777fcf
AleksandrBychuk <sbychuk645@gmail.com>
2022-04-06 22:49:44 +0300

    adapatation validation adapter adresses

--- TLabs.ExchangeSdk/CryptoAdapters/CryptoAddressesHelper.cs
+++ TLabs.ExchangeSdk/CryptoAdapters/CryptoAddressesHelper.cs
@@ -9,9 +9,15 @@ namespace TLabs.ExchangeSdk.CryptoAdapters
 {
     public static class CryptoAddressesHelper
     {
-        public static bool IsTronAddress(string address)
+        public static bool IsValidAddress(string adapterCode)
         {
-            return new Regex("T[A-Za-z1-9]{33}").IsMatch(address);
+            return adapterCode switch
+            {
+                "btc" => new Regex("/^0x[a-fA-F0-9]{40}$/").IsMatch(adapterCode),
+                "eth" => new Regex("/^0x[a-fA-F0-9]{40}$/").IsMatch(adapterCode),
+                "trx" => new Regex("T[A-Za-z1-9]{33}").IsMatch(adapterCode),
+                _ => false
+            };
         }
     }
 }
