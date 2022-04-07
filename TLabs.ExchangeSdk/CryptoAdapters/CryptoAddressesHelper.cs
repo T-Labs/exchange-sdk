01ec2046e1b9bd836efeb4ba93f9b004f8109147
AleksandrBychuk <sbychuk645@gmail.com>
2022-04-07 17:45:29 +0300

    fix

--- TLabs.ExchangeSdk/CryptoAdapters/CryptoAddressesHelper.cs
+++ TLabs.ExchangeSdk/CryptoAdapters/CryptoAddressesHelper.cs
@@ -9,13 +9,13 @@ namespace TLabs.ExchangeSdk.CryptoAdapters
 {
     public static class CryptoAddressesHelper
     {
-        public static bool IsValidAddress(string adapterCode)
+        public static bool IsValidAddress(string adapterCode, string adapterAddress)
         {
             return adapterCode switch
             {
-                "btc" => new Regex("/^0x[a-fA-F0-9]{40}$/").IsMatch(adapterCode),
-                "eth" => new Regex("/^0x[a-fA-F0-9]{40}$/").IsMatch(adapterCode),
-                "trx" => new Regex("T[A-Za-z1-9]{33}").IsMatch(adapterCode),
+                "btc" => new Regex("^0x[a-fA-F0-9]{40}$").IsMatch(adapterAddress),
+                "eth" => new Regex("^0x[a-fA-F0-9]{40}$").IsMatch(adapterAddress),
+                "trx" => new Regex("T[A-Za-z1-9]{33}").IsMatch(adapterAddress),
                 _ => false
             };
         }
