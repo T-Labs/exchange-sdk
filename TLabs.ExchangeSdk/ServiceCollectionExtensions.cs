666890feb7fb5a6354f137f695849bcccfbb3718
Vitaliy <vitaliy.lebedev7@gmail.com>
2023-08-08 02:31:10 +0600

    fix service

--- TLabs.ExchangeSdk/ServiceCollectionExtensions.cs
+++ TLabs.ExchangeSdk/ServiceCollectionExtensions.cs
@@ -32,7 +32,7 @@ namespace TLabs.ExchangeSdk
             services.AddTransient<Trading.ClientMatchingEngine>();
             services.AddTransient<Trading.ClientTradingBrokerage>();
             services.AddTransient<Users.ClientUsers>();
-            services.AddTransient<Users.IIPService>();
+            services.AddTransient<Users.IIPService, Users.IPService>();
             services.AddTransient<Verification.ClientVerifications>();
             services.AddTransient<Withdrawals.ClientWithdrawals>();
 
