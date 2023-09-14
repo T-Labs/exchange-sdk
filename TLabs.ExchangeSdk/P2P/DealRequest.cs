6fae8f21376121d9ba14fadd5bf696600216396e
Igor <47169805+IgorDevR@users.noreply.github.com>
2023-09-14 17:11:45 +0600

    Changing the field access modifier DealRequest, OrderRequest. Updated version.

--- TLabs.ExchangeSdk/P2P/DealRequest.cs
+++ TLabs.ExchangeSdk/P2P/DealRequest.cs
@@ -5,10 +5,10 @@ namespace TLabs.ExchangeSdk.P2P;
 
 public class DealRequest
 {
-    public Guid OrderId { get; init; }
-    public string PaymentMethodCurrencyCode { get; init; }
-    public string DealUserId { get; init; }
-    public DateTimeOffset? DatePaymentSystemSent { get; init; }
-    public DateTimeOffset? DateCryptoReleased { get; init; }
-    public DateTimeOffset? DateFinished { get; init; }
+    public Guid OrderId { get; set; }
+    public string PaymentMethodCurrencyCode { get; set; }
+    public string DealUserId { get; set; }
+    public DateTimeOffset? DatePaymentSystemSent { get; set; }
+    public DateTimeOffset? DateCryptoReleased { get; set; }
+    public DateTimeOffset? DateFinished { get; set; }
 }
