2df26621a5e0b298ce47c5acccdd1ac92cefaf94
Igor <47169805+IgorDevR@users.noreply.github.com>
2023-11-10 20:56:03 +0600

    changing InvoiceCreateResponse

--- TLabs.ExchangeSdk/Bwp/InvoiceCreateResponse.cs
+++ TLabs.ExchangeSdk/Bwp/InvoiceCreateResponse.cs
@@ -2,8 +2,8 @@ namespace TLabs.ExchangeSdk.Bwp;
 
 public class InvoiceCreateResponse
 {
+    public string ErrorCode { get; set; }
     public long InvoiceId { get; set; }
     public string CardNumber { get; set; }
     public string PaymentMethodKey { get; set; }
-    public string ErrorCode { get; set; }
 }
