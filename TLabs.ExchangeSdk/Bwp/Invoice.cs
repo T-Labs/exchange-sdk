6235f21a4c071cf7c1d2336bf6a4b06d48fc99ee
Igor <47169805+IgorDevR@users.noreply.github.com>
2023-11-10 20:03:45 +0600

    changing bwp Invoice model

--- TLabs.ExchangeSdk/Bwp/Invoice.cs
+++ TLabs.ExchangeSdk/Bwp/Invoice.cs
@@ -7,17 +7,17 @@ public class Invoice
     public long BwpInvoiceId { get; set; }
     public long MerchantId { get; set; }
     public decimal Amount { get; set; }
-    public string CurrencyCode { get; set; } = "RUB";
+    public string CurrencyCode { get; set; }
     public string CallbackUrl { get; set; }
     public string PaymentMethodCode { get; set; }
 
     public DateTimeOffset Created { get; set; }
     public DateTimeOffset Expires { get; set; }
 
-    public string? PaymentSystemInvoiceId { get; set; }
-    public string? PaymentUrl { get; set; }
+    public string PaymentSystemInvoiceId { get; set; }
+    public string PaymentUrl { get; set; }
 
-    public string? RedirectUrl { get; set; }
+    public string RedirectUrl { get; set; }
 
-    public bool IsExpired => Expires < DateTimeOffset.UtcNow;
+    public bool IsExpired { get; set; }
 }
