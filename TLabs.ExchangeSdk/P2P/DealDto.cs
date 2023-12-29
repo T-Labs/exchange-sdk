95b632c01c34a6b25db4e315081ed9f724062305
Igor <47169805+IgorDevR@users.noreply.github.com>
2023-12-29 16:37:54 +0700

    changed p2p client and models. updated version

--- TLabs.ExchangeSdk/P2P/DealDto.cs
+++ TLabs.ExchangeSdk/P2P/DealDto.cs
@@ -1,12 +1,12 @@
-using RabbitMQ.Client;
 using System;
 
 namespace TLabs.ExchangeSdk.P2P;
 
-public class DealRequest
+public record DealDto
 {
     public Guid OrderId { get; set; }
     public string PaymentMethodCurrencyCode { get; set; }
+
     public string DealUserId { get; set; }
     public DateTimeOffset? DatePaymentSystemSent { get; set; }
     public DateTimeOffset? DateCryptoReleased { get; set; }
--- TLabs.ExchangeSdk/P2P/OrderDto.cs
+++ TLabs.ExchangeSdk/P2P/OrderDto.cs
@@ -1,19 +1,27 @@
-using RabbitMQ.Client;
 using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace TLabs.ExchangeSdk.P2P;
 
-public class OrderRequest
+public class OrderDto
 {
     public string UserId { get; set; }
+
+    /// <summary> Buying on the exchange from a payment system or selling from the exchange </summary>
     public bool IsBuyingOnExchange { get; set; }
+
     public string ExchangeCurrencyCode { get; set; }
+
     public decimal Price { get; set; }
+
     public decimal TotalOrderAmount { get; set; }
+
     public decimal MinDealAmount { get; set; }
+
     public decimal MaxDealAmount { get; set; }
+
     public DateTimeOffset? DateClosed { get; set; }
+
     public string Description { get; set; }
+
     public int MaxTimeMinutes { get; set; }
 }
