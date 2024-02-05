6dce1292c869035cf3d129c507e36a05e8c4d67a
whygizzmo <mrcool200275@gmail.com>
2024-02-05 21:59:25 +0600

    add action GetUserOrdersInfo

---
+++ TLabs.ExchangeSdk/P2P/UserInfoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TLabs.ExchangeSdk.P2P;
+
+public class UserInfoDto
+{
+    public int CompletedBuys { get; set; }
+    public int CompletedSells { get; set; }
+    public int HighRatingBuys { get; set; }
+    public int HighRatingSells { get; set; }
+    public int CompletedDeals30Days { get; set; }
+    public int AllDeals30Days { get; set; }
+}
