71b9ee45b3e0cc42c8f9ff7017e2de0398c56d4c
Vitaliy <vitaliy.lebedev7@gmail.com>
2024-02-25 23:12:47 +0600

    add calculated property

--- TLabs.ExchangeSdk/P2P/UserInfoDto.cs
+++ TLabs.ExchangeSdk/P2P/UserInfoDto.cs
@@ -1,3 +1,5 @@
+using TLabs.DotnetHelpers;
+
 namespace TLabs.ExchangeSdk.P2P;
 
 public class UserInfoDto
@@ -9,4 +11,8 @@ public class UserInfoDto
     public int HighRatingSells { get; set; }
     public int CompletedDeals30Days { get; set; }
     public int AllDeals30Days { get; set; }
+
+    public decimal CompletedDealsPercentage30Days => AllDeals30Days == 0
+        ? 100
+        : ((decimal)CompletedDeals30Days / AllDeals30Days).RoundDown(2);
 }
