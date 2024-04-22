bcdc0e78ad29a035ea7dd282d7e534c4cce4a1f5
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-04-22 17:34:28 +0600

    Add ClientWebapp, P2PNotification
    Added classes for deal change notifications

---
+++ TLabs.ExchangeSdk/WebApp/ClientWebapp.cs
@@ -0,0 +1,16 @@
+using Flurl.Http;
+using System.Threading.Tasks;
+using TLabs.DotnetHelpers;
+using TLabs.ExchangeSdk.P2P.Deals;
+
+namespace TLabs.ExchangeSdk.WebApp;
+
+public class ClientWebapp
+{
+    public async Task<IFlurlResponse> SendP2PDealNotification(P2PNotification notification)
+    {
+        var result = await $"webapp/p2p-deal/notify".InternalApi()
+            .PostJsonAsync(notification);
+        return result;
+    }
+}
