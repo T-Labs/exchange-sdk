2d07ba76c13c46257315055aa47f110e8dd048c8
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-04-22 20:11:54 +0600

    Change ClientWebapp, update app version
     - ClientWebapp, changed route in the SendP2PDealNotification method
     - Updated app version

--- TLabs.ExchangeSdk/WebApp/ClientWebapp.cs
+++ TLabs.ExchangeSdk/WebApp/ClientWebapp.cs
@@ -9,7 +9,7 @@ public class ClientWebapp
 {
     public async Task<IFlurlResponse> SendP2PDealNotification(P2PNotification notification)
     {
-        var result = await $"webapp/p2p-deal/notify".InternalApi()
+        var result = await $"webapp/p2p/deals/notify".InternalApi()
             .PostJsonAsync(notification);
         return result;
     }
