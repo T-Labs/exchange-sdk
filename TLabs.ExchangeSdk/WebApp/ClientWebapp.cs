8ab399368d78b0c58059907ca1d30f624c841a84
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-04-23 12:05:33 +0600

    Change ClientWebapp, P2PNotification, update app version
     - ClientWebapp, changed route
     - P2PNotification, added P2PChat target, added constructors.
     - Updated app version

--- TLabs.ExchangeSdk/WebApp/ClientWebapp.cs
+++ TLabs.ExchangeSdk/WebApp/ClientWebapp.cs
@@ -1,7 +1,6 @@
 using Flurl.Http;
 using System.Threading.Tasks;
 using TLabs.DotnetHelpers;
-using TLabs.ExchangeSdk.P2P.Deals;
 
 namespace TLabs.ExchangeSdk.WebApp;
 
@@ -9,7 +8,7 @@ public class ClientWebapp
 {
     public async Task<IFlurlResponse> SendP2PDealNotification(P2PNotification notification)
     {
-        var result = await $"webapp/p2p/deals/notify".InternalApi()
+        var result = await $"webapp/p2p/notify".InternalApi()
             .PostJsonAsync(notification);
         return result;
     }
--- TLabs.ExchangeSdk/WebApp/P2PNotification.cs
+++ TLabs.ExchangeSdk/WebApp/P2PNotification.cs
@@ -6,9 +6,20 @@ public class P2PNotification
 {
     public string Target { get; set; }
     public Deal Deal { get; set; }
+
+    public P2PNotification()
+    {
+    }
+
+    public P2PNotification(string target, Deal deal)
+    {
+        Target = target;
+        Deal = deal;
+    }
 }
 
 public static class P2PNotificationTarget
 {
     public const string P2PDeal = "P2PDeal";
+    public const string P2PChat = "P2PChat";
 }
