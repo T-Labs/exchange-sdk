ee7dfbd26fb52c38b5f54d2ed3c3ed7761e3d6d4
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-05-06 14:26:47 +0600

    Remove unnecessary comments

--- TLabs.ExchangeSdk/Verification/TypeProofOfAddress.cs
+++ TLabs.ExchangeSdk/Verification/TypeProofOfAddress.cs
@@ -2,19 +2,8 @@ namespace TLabs.ExchangeSdk.Verification
 {
     public class TypeProofOfAddress
     {
-        /// <summary>
-        /// Number in database
-        /// </summary>
         public int Id { get; set; }
-
-        /// <summary>
-        /// Name
-        /// </summary>
         public string Name { get; set; }
-
-        /// <summary>
-        /// Name in English
-        /// </summary>
         public string Name_EN { get; set; }
     }
 }
