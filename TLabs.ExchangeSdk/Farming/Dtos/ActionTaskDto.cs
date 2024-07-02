4033e708f4edda1f88d55846d4c5453cd4319ba6
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-07-02 14:09:52 +0600

    Fix ActionTaskDto

--- TLabs.ExchangeSdk/Farming/Dtos/ActionTaskDto.cs
+++ TLabs.ExchangeSdk/Farming/Dtos/ActionTaskDto.cs
@@ -2,9 +2,8 @@ namespace TLabs.ExchangeSdk.Farming.Dtos;
 
 public class ActionTaskDto
 {
-    public int TgUserId { get; set; }
+    public long TenantId { get; set; }
     public string Description { get; set; }
-    public long RewardId { get; set; }
     public string ActionUrl { get; set; }
     public decimal RewardAmount { get; set; }
     public PlatformType Platform { get; set; }
