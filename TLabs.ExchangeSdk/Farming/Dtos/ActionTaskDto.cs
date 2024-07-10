834d780b0f4e0a11f97e76f503d49010abd6d9f9
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-07-10 13:35:11 +0600

    Change ActionTask, ActionTaskDto, update app version
     - ActionTask, renamed Priority to Ordering, added AvailableDayNumber.
     - ActionTaskDto, renamed Priority to Ordering, added AvailableDayNumber.
     - Updated app version.

--- TLabs.ExchangeSdk/Farming/Dtos/ActionTaskDto.cs
+++ TLabs.ExchangeSdk/Farming/Dtos/ActionTaskDto.cs
@@ -2,11 +2,12 @@ namespace TLabs.ExchangeSdk.Farming.Dtos;
 
 public class ActionTaskDto
 {
-    public int Priority { get; set; }
+    public int Ordering { get; set; }
     public long TenantId { get; set; }
     public string Description { get; set; }
     public string ActionUrl { get; set; }
     public decimal RewardAmount { get; set; }
     public PlatformType Platform { get; set; }
     public bool IsActive { get; set; }
+    public int? AvailableDayNumber { get; set; }
 }
