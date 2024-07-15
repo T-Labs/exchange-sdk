6f9d732db82e29bdd68fcc97c67145d84d29d769
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-07-15 22:31:51 +0600

    Change ClientFarming, UploadImageDto, update app version
     - ClientFarming, added GetRewardByType.
     - Updated app version.

--- TLabs.ExchangeSdk/Farming/Dtos/UploadImageDto.cs
+++ TLabs.ExchangeSdk/Farming/Dtos/UploadImageDto.cs
@@ -3,9 +3,5 @@ namespace TLabs.ExchangeSdk.Farming.Dtos;
 public class UploadImageDto
 {
     public string Extension { get; set; }
-
     public byte[] Data { get; set; }
-    public long? TgUserId { get; set; }
-    public long? TenantId { get; set; }
-    public string OldImageId { get; set; }
 }
