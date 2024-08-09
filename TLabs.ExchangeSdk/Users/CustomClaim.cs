bbc0632d159f2f4d3b0ace48e4c0907ec728aebe
Vitaliy <vitaliy.lebedev7@gmail.com>
2024-08-09 20:27:14 +0500

    add claims request, update CustomClaim

--- TLabs.ExchangeSdk/Users/CustomClaim.cs
+++ TLabs.ExchangeSdk/Users/CustomClaim.cs
@@ -1,18 +1,19 @@
 namespace TLabs.ExchangeSdk.Users
 {
     /// <summary>
-    /// only needed because identityserver Claim objects do not deserialize
+    /// needed because identityserver Claim objects do not deserialize
     /// </summary>
     public class CustomClaim
     {
-        public CustomClaim(string type, string value)
+        public CustomClaim(string type, string value, string userId)
         {
             Type = type;
             Value = value;
+            UserId = userId;
         }
 
         public string Type { get; set; }
-
         public string Value { get; set; }
+        public string UserId { get; set; }
     }
 }
