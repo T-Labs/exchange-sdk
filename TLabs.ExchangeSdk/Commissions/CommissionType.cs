2d6fee397f45986c06a6d6d0fc7b8f9f1b45090e
Vitaliy <vitaliy.lebedev7@gmail.com>
2024-10-26 05:08:15 +0500

    add new CommissionTypes, add p2p GetCurrencyPairs

--- TLabs.ExchangeSdk/Commissions/CommissionType.cs
+++ TLabs.ExchangeSdk/Commissions/CommissionType.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace TLabs.ExchangeSdk.Commissions
@@ -27,9 +28,17 @@ namespace TLabs.ExchangeSdk.Commissions
         [Required]
         public string ValueKey { get; set; }
 
-        public static readonly CommissionType Refill = new CommissionType("1", "Пополнение", "Refill");
-        public static readonly CommissionType Withdrawal = new CommissionType("2", "Вывод", "Withdrawal");
-        public static readonly CommissionType DealBid = new CommissionType("3", "Сделка покупка", "DealBid");
-        public static readonly CommissionType DealAsk = new CommissionType("4", "Сделка продажа", "DealAsk");
+        public static readonly CommissionType Refill = new CommissionType("1", "Пополнение", nameof(Refill));
+        public static readonly CommissionType Withdrawal = new CommissionType("2", "Вывод", nameof(Withdrawal));
+        public static readonly CommissionType DealBid = new CommissionType("3", "Сделка покупка", nameof(DealBid));
+        public static readonly CommissionType DealAsk = new CommissionType("4", "Сделка продажа", nameof(DealAsk));
+
+        /// <summary>When P2P merchant buys crypto</summary>
+        public static readonly CommissionType P2pBuy = new CommissionType("50", "P2P покупка", nameof(P2pBuy));
+
+        /// <summary>When P2P merchant sells crypto</summary>
+        public static readonly CommissionType P2pSell = new CommissionType("60", "P2P продажа", nameof(P2pSell));
+
+        public static List<string> TypesUsingCurrencyPair = new List<string> { P2pBuy.Code, P2pSell.Code };
     }
 }
