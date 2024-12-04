42f7ac367d2ef112ef339e2e7d73dcc4ced7404f
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-12-04 15:47:54 +0600

    Change CurrencyPairTradingVolume, update app version
     - CurrencyPairTradingVolume, added TotalExchangeAmount and
    TotalPaymentAmount fields.
     - Updated app version.

--- TLabs.ExchangeSdk/P2P/CurrencyPairTradingVolume.cs
+++ TLabs.ExchangeSdk/P2P/CurrencyPairTradingVolume.cs
@@ -1,8 +1,10 @@
-﻿namespace TLabs.ExchangeSdk.P2P;
+namespace TLabs.ExchangeSdk.P2P;
 
 public class CurrencyPairTradingVolume
 {
-    public string PaymentCurrencyCode { get; set; }
     public string ExchangeCurrencyCode { get; set; }
+    public string PaymentCurrencyCode { get; set; }
+    public string TotalExchangeAmount { get; set; }
+    public string TotalPaymentAmount { get; set; }
     public decimal TotalAmountInUsdt { get; set; }
 }
