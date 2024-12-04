ef3d4b2f8ff7ae7350f40941bb5b37e4107ad896
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-12-04 15:50:56 +0600

    Change CurrencyPairTradingVolume
    - Fixed property type.

--- TLabs.ExchangeSdk/P2P/CurrencyPairTradingVolume.cs
+++ TLabs.ExchangeSdk/P2P/CurrencyPairTradingVolume.cs
@@ -4,7 +4,7 @@ public class CurrencyPairTradingVolume
 {
     public string ExchangeCurrencyCode { get; set; }
     public string PaymentCurrencyCode { get; set; }
-    public string TotalExchangeAmount { get; set; }
-    public string TotalPaymentAmount { get; set; }
+    public decimal TotalExchangeAmount { get; set; }
+    public decimal TotalPaymentAmount { get; set; }
     public decimal TotalAmountInUsdt { get; set; }
 }
