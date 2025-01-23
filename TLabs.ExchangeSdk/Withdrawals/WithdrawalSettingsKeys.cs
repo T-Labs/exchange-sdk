6514627da7cb267e2b628e3cfd17cf0be34b568a
IgorDevR <IgorBrezhnev99@gmail.com>
2025-01-23 17:51:00 +0600

    Rename UserConstants to UserSettingsKeys.
     - Removed WithdrawalSettingsKeys.
     - Updated app version.

--- TLabs.ExchangeSdk/Withdrawals/WithdrawalSettingsKeys.cs
+++
@@ -1,7 +0,0 @@
-namespace TLabs.ExchangeSdk.Withdrawals;
-
-public class WithdrawalSettingsKeys
-{
-    public const string VerificationRequired = "VerificationRequired";
-    public const string MinimumVerificationStage = "MinimumVerificationStage";
-}
