070e5670b840ff36b829428218ac0b53fc1acc84
Vitaliy <vitaliy.lebedev7@gmail.com>
2025-05-15 13:00:23 +0500

    add FairLaunch ClientType

--- TLabs.ExchangeSdk/ClientType.cs
+++ TLabs.ExchangeSdk/ClientType.cs
@@ -2,6 +2,11 @@ namespace TLabs.ExchangeSdk
 {
     public enum ClientType
     {
-        User = 0, LiquidityBot = 1, DealsBot = 2, AffiliateBot = 3, ExchangerBot = 40
+        User = 0,
+        LiquidityBot = 1,
+        DealsBot = 2,
+        AffiliateBot = 3,
+        ExchangerBot = 40,
+        CurrencyListingFairLaunchBot = 50,
     }
 }
