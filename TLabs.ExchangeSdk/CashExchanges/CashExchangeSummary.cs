93375640b733a0f56223f7fcabd31a4475e1dec0
CITRUSR <savelypetrov2007@mail.ru>
2025-07-31 19:16:59 +0300

    add get summary method

---
+++ TLabs.ExchangeSdk/CashExchanges/CashExchangeSummary.cs
@@ -0,0 +1,17 @@
+namespace TLabs.ExchangeSdk.CashExchanges;
+
+public class CashExchangeSummary
+{
+    public decimal TrustUsdtSum { get; set; }
+    public decimal TronlinkUsdtSum { get; set; }
+    public decimal BybitRUSDTSum { get; set; }
+    public decimal TrustTrxSum { get; set; }
+    public decimal TronlinkTrxSum { get; set; }
+    public decimal UsdSum { get; set; }
+    public decimal TrySum { get; set; }
+    public decimal EurSum { get; set; }
+    public decimal TrxInUsdt { get; set; }
+    public decimal UsdInUsdt { get; set; }
+    public decimal TryInUsdt { get; set; }
+    public decimal EurInUsdt { get; set; }
+}
--- TLabs.ExchangeSdk/CashExchanges/ClientCashExchanges.cs
+++ TLabs.ExchangeSdk/CashExchanges/ClientCashExchanges.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Flurl;
 using Flurl.Http;
 using TLabs.DotnetHelpers;
 
@@ -17,5 +19,13 @@ public class ClientCashExchanges
     public async Task CreateAsync(CashExchange cashExchange)
     {
         await BASE_EXCHANGE_URL.InternalApi().PostJsonAsync(cashExchange);
+    }
+
+    public async Task<CashExchangeSummary> GetSummaryAsync(DateTimeOffset date)
+    {
+        return await $"{BASE_EXCHANGE_URL}/summary"
+            .SetQueryParam(nameof(date), date)
+            .InternalApi()
+            .GetJsonAsync<CashExchangeSummary>();
     }
 }
