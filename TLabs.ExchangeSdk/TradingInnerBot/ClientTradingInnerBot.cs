343c332713426ed10f804128b09b959853c3ffcb
CITRUSR <savelypetrov2007@mail.ru>
2025-08-25 13:09:43 +0300

    fix method in client tradingInnerBot

--- TLabs.ExchangeSdk/TradingInnerBot/ClientTradingInnerBot.cs
+++ TLabs.ExchangeSdk/TradingInnerBot/ClientTradingInnerBot.cs
@@ -37,6 +37,6 @@ public class ClientTradingInnerBot
 
     public async Task SendDealEventAsync(MatchingDeal deal)
     {
-        await $"{EVENTS_URL}/trade".PostJsonAsync(deal);
+        await $"{EVENTS_URL}/trade".InternalApi().PostJsonAsync(deal);
     }
 }
