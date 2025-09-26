95fb3484a862551851666cfa37ba7a1652b3863a
CITRUSR <savelypetrov2007@mail.ru>
2025-09-26 09:33:22 +0300

    update client types

--- TLabs.ExchangeSdk/ClientType.cs
+++ TLabs.ExchangeSdk/ClientType.cs
@@ -3,12 +3,12 @@ namespace TLabs.ExchangeSdk
     public enum ClientType
     {
         User = 0, // Created by real user
-        LiquidityBot = 1, // LiquidityImport from other Exchange (Binance, Okex, etc)
+        LiquidityImport = 1, // LiquidityImport from other Exchange (Binance, Okex, etc)
         DealsBot = 2, // InnerTradingBot (creates deals with itself)
         AffiliateBot = 3, // Affiliate program exchanges other assets into USDT
         ExchangerBot = 40, // Exchanger service (not used currently)
         CurrencyListingFairLaunchBot = 50, // LiquidityPool (FairLaunch) creates orderbook for new tokens
         TradingBot = 60, // Creates market orders to build trading chart
-        LiqBot = 70, // Creates orderbooks
+        LiquidityBot = 70, // Creates orderbooks
     }
 }
