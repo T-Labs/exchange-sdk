5e16c70291c2aac19ac1937f22cfb4db12917995
Vitaliy <vitaliy.lebedev7@gmail.com>
2025-09-29 04:50:24 +0500

    update CashExchange model

--- TLabs.ExchangeSdk/CashExchanges/CashExchange.cs
+++ TLabs.ExchangeSdk/CashExchanges/CashExchange.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using TLabs.ExchangeSdk.CashHandover;
 
 namespace TLabs.ExchangeSdk.CashExchanges;
@@ -6,15 +7,27 @@ namespace TLabs.ExchangeSdk.CashExchanges;
 public class CashExchange
 {
     public Guid Id { get; set; }
+
+    /// <summary>Sheets row number</summary>
+    public int? RowId { get; set; }
+
+    /// <summary>if deal number is null it means that exchange is correction</summary>
+    public int? DealNumber { get; set; }
+
     public string PersonName { get; set; }
     public CashHandoverClient Client { get; set; }
     public Guid? ClientId { get; set; }
     public string Comment { get; set; }
-    public int? DealNumber { get; set; }
     public DateTimeOffset Created { get; set; }
     public Guid? DocumentPhotoId { get; set; }
-    public ImageDto DocumentPhotoDto { get; set; }
+
+    /// <summary>Using for corrections</summary>
+    public bool IsDeleted { get; set; }
+
     public CashHandoverRequestStatus Status { get; set; }
+
+    /// <summary>null if Spread wasn't filled in Sheets</summary>
+    public decimal? SpreadFromSheets { get; set; }
 
     public decimal? TrustUsdtIn { get; set; }
     public decimal? TrustUsdtOut { get; set; }
@@ -32,4 +45,14 @@ public class CashExchange
     public decimal? BybitRUSDTOut { get; set; }
     public decimal? TrustTRXIn { get; set; }
     public decimal? TrustTRXOut { get; set; }
+
+    //corrections section
+    public decimal? CorrectionUsdtValue { get; set; }
+    public decimal? CorrectionTrxValue { get; set; }
+    public decimal? CorrectionUsdValue { get; set; }
+    public decimal? CorrectionTryValue { get; set; }
+    public decimal? CorrectionEurValue { get; set; }
+
+    [NotMapped]
+    public ImageDto DocumentPhotoDto { get; set; }
 }
