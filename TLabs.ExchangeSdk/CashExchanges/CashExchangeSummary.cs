d6ac70132afed6c8047c1289c6b17cbde98004e9
Vitaliy <vitaliy.lebedev7@gmail.com>
2025-10-03 20:12:52 +0500

    update CashExchangeSummary model

--- TLabs.ExchangeSdk/CashExchanges/CashExchangeSummary.cs
+++ TLabs.ExchangeSdk/CashExchanges/CashExchangeSummary.cs
@@ -1,15 +1,23 @@
+using System;
+
 namespace TLabs.ExchangeSdk.CashExchanges;
 
 public class CashExchangeSummary
 {
-    public decimal TrustUsdtSum { get; set; }
-    public decimal TronlinkUsdtSum { get; set; }
-    public decimal BybitRUSDTSum { get; set; }
-    public decimal TrustTrxSum { get; set; }
-    public decimal TronlinkTrxSum { get; set; }
-    public decimal UsdSum { get; set; }
-    public decimal TrySum { get; set; }
-    public decimal EurSum { get; set; }
-    public decimal SumOfUsdt { get; set; }
+    public DateTimeOffset Date { get; set; }
     public decimal PrevDayRemainsInUsdt { get; set; }
+
+    public decimal TronlinkUsdtSum { get; set; }
+    public decimal BlackUsdtSum { get; set; }
+    public decimal UsdSum { get; set; }
+    public decimal EurSum { get; set; }
+    public decimal TrySum { get; set; }
+    public decimal BybitRUSDTSum { get; set; }
+    public decimal TronlinkTrxSum { get; set; }
+    public decimal BlackTrxSum { get; set; }
+    public decimal TrustUsdtSum { get; set; }
+    public decimal TrustTrxSum { get; set; }
+
+    /// <summary>Calculated in Admin</summary>
+    public decimal SumOfUsdt { get; set; }
 }
