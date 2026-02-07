158a44165ed4a0254dd0896dcf2a6cf471c51833
Vitaliy <vitaliy.lebedev7@gmail.com>
2026-02-07 23:12:16 +0500

    update ProfitType

--- TLabs.ExchangeSdk/Affiliate/ProfitType.cs
+++ TLabs.ExchangeSdk/Affiliate/ProfitType.cs
@@ -2,10 +2,10 @@ namespace TLabs.ExchangeSdk.Affiliate
 {
     public enum ProfitType
     {
-        TariffPurchase = 0,
         DealCommission = 1,
         WithdrawalCommission = 2,
         CurrencyListing = 40,
         ApiTradeProfitPayment = 50,
+        StakeAccrual = 100,
     }
 }
