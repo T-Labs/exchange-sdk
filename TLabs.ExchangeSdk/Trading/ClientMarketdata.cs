df21a66f880e395d85c4bbddac481a4354eb694a
Vitaliy <vitaliy.lebedev7@gmail.com>
2026-02-13 22:47:21 +0500

    fix

--- TLabs.ExchangeSdk/Trading/ClientMarketdata.cs
+++ TLabs.ExchangeSdk/Trading/ClientMarketdata.cs
@@ -146,7 +146,7 @@ namespace TLabs.ExchangeSdk.Trading
 
         public async Task<decimal> GetTotalDailyVolumeUsdt()
         {
-            var result = await $"api/deals/total-daily-volume-usdt".InternalApi()
+            var result = await $"marketdata/deals/total-daily-volume-usdt".InternalApi()
                 .GetJsonAsync<decimal>();
             return result;
         }
