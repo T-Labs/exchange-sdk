b1ad09f485a404e2c2d45d69bb2b39dfd9c71066
CITRUSR <savelypetrov2007@mail.ru>
2026-02-19 16:26:47 +0300

    feat(VerificationUser): add email prop for notification using

--- TLabs.ExchangeSdk/Verification/VerificationUser.cs
+++ TLabs.ExchangeSdk/Verification/VerificationUser.cs
@@ -15,6 +15,7 @@ namespace TLabs.ExchangeSdk.Verification
         public string Name { get; set; }
         public string SecondName { get; set; }
         public string MiddleName { get; set; }
+        public string Email { get; set; }
 
         public int? CitizenshipId { get; set; }
         public Citizenship Citizenship { get; set; }
