e7acc6f02806a9bf7d0699ffe8b979cf80bae57d
krzeeeed <richismyho420@mail.ru>
2026-05-08 18:56:05 +0300

    add vip status feat

---
+++ TLabs.ExchangeSdk/Users/VipStatusDto.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace TLabs.ExchangeSdk.Users
+{
+    public class VipStatusDto
+    {
+        public bool IsVip { get; set; }
+        public DateTimeOffset? VipActivatedAt { get; set; }
+        public DateTimeOffset? VipGracePeriodEndsAt { get; set; }
+        public bool IsVipAvatarSelected { get; set; }
+    }
+
+    public class SetVipStatusDto
+    {
+        public bool IsVip { get; set; }
+        public DateTimeOffset? VipGracePeriodEndsAt { get; set; }
+    }
+
+    public class VipAssistantDto
+    {
+        public string Name { get; set; }
+    }
+}
