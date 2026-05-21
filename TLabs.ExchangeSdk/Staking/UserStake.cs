5d5d7343d3d9363f4e8eff1a1e04cfcc5e5e60a8
Roman Shreiter <ukkitop@gmail.com>
2026-05-21 15:06:25 +0300

    remove field

--- TLabs.ExchangeSdk/Staking/UserStake.cs
+++ TLabs.ExchangeSdk/Staking/UserStake.cs
@@ -47,9 +47,6 @@ namespace TLabs.ExchangeSdk.Staking
 
         public DateTimeOffset DateLastAccrual { get; set; }
 
-
-        public decimal SingleAccrualRate => YearlyAccrualPercentage / 100m / 365;
-
         public override string ToString() => $"{nameof(UserStake)}(Id:{Id}, userId:{UserId}, status:{Status}, settingId:{StakingSettingId}, " +
             $"amount:{Amount}, yearlyRate:{YearlyAccrualPercentage}%, singleAccrual:{SingleAccrualAmount}, singleAccrualUsdt:{SingleAccrualAmountUsdt}, " +
             $"{DateStarted:s} - {DateEnding:s}, released:{DateReleased}, lastAccrual:{DateLastAccrual:s})";
