7eb1b16deea5200aa073e3f8e2271d98c6e54a96
Vitaliy <vitaliy.lebedev7@gmail.com>
2026-06-19 18:49:20 +0500

    aml: remove AmlReturn withdrawal type, bump version

--- TLabs.ExchangeSdk/Withdrawals/WithdrawalType.cs
+++ TLabs.ExchangeSdk/Withdrawals/WithdrawalType.cs
@@ -6,7 +6,5 @@ namespace TLabs.ExchangeSdk.Withdrawals
     public enum WithdrawalType
     {
         Crypto = 0, AdvCashInternal = 10, AdvCashYandex = 20, AdvCashQiwi = 30, AdvCashBankCard = 40, Swift = 50, Cash = 60,
-        /// <summary>System-initiated send back to the depositor's address after an AML hold.</summary>
-        AmlReturn = 70,
     }
 }
