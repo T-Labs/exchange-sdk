9e3880ecb31cb76ffc0a1e47ef0ce8eb3e355651
Vitaliy <vitaliy.lebedev7@gmail.com>
2026-06-29 17:27:31 +0500

    staking affiliate freeze level dto

--- TLabs.ExchangeSdk/Affiliate/StakingAffiliate/StakingAffiliateAdminUserRowDto.cs
+++ TLabs.ExchangeSdk/Affiliate/StakingAffiliate/StakingAffiliateAdminUserRowDto.cs
@@ -9,7 +9,10 @@ namespace TLabs.ExchangeSdk.Affiliate.StakingAffiliate
         public string Nickname { get; set; }
         public string Email { get; set; }
         public string CurrentLevel { get; set; }
-        public DateTimeOffset? LevelRecalculationFrozenUntil { get; set; }
+
+        /// <summary>The active freeze floor (null when there is no freeze). May be below or above <see cref="CurrentLevel"/>.</summary>
+        public string FreezeLevel { get; set; }
+        public DateTimeOffset? LevelFrozenUntil { get; set; }
         public int? RemainingFreezeDays { get; set; }
     }
 }
