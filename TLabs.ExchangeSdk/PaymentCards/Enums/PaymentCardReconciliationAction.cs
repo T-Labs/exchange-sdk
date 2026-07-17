7ef0ef2c8eebd65ab0ec7bd0267724f21ecb3bfb
elizarov <iamvizar@mail.ru>
2026-07-17 12:50:17 +0300

    fix: handle payment card reconciliation actions correctly

---
+++ TLabs.ExchangeSdk/PaymentCards/Enums/PaymentCardReconciliationAction.cs
@@ -0,0 +1,15 @@
+﻿namespace TLabs.ExchangeSdk.PaymentCards.Enums;
+
+
+public enum PaymentCardReconciliationAction
+{
+    /// <summary>
+    /// Повторить финальное зачисление/списание в Depository.
+    /// </summary>
+    Complete = 10,
+
+    /// <summary>
+    /// Повторить компенсирующую отмену TopUp.
+    /// </summary>
+    Cancel = 20
+}
