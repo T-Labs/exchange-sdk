ac5821419c23733b9b160eebc81f79b9d708c48c
elizarov <iamvizar@mail.ru>
2026-07-23 19:08:28 +0300

    feat: add verification reminder notificator client

---
+++ TLabs.ExchangeSdk/Notificator/VerificationReminderDispatchResponse.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace TLabs.ExchangeSdk.Notificator
+{
+    public class VerificationReminderDispatchResponse
+    {
+        public int UnverifiedUsersCount { get; set; }
+
+        public int EligibleUsersCount { get; set; }
+
+        public int EnqueuedCount { get; set; }
+
+        public int SkippedWithoutEmailCount { get; set; }
+
+        public int SkippedDeletedCount { get; set; }
+
+        public int SkippedLockedCount { get; set; }
+
+        public List<Guid> DispatchIds { get; set; } = new List<Guid>();
+    }
+}
