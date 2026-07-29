ef2267335a535913a5d8e719443b64ee8c7ca64b
elizarov <iamvizar@mail.ru>
2026-07-29 11:40:54 +0300

    fix conflicts

--- TLabs.ExchangeSdk/Depository/ClientDepository.cs
+++ TLabs.ExchangeSdk/Depository/ClientDepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Flurl.Http;
 using TLabs.DotnetHelpers;
@@ -100,14 +101,16 @@ namespace TLabs.ExchangeSdk.Depository
         }
 
         public virtual async Task<PagedList<TransactionDto>> GetTransactionsByAccountIdPaged(Guid accountId,
-            int page = 1, int pageSize = 50)
+            int page = 1, int pageSize = 50, CancellationToken cancellationToken = default)
         {
             var request = "depository/transaction/by-account/paged".InternalApi()
-                .SetQueryParam(nameof(accountId), accountId).SetQueryParam(nameof(page), page)
+                .SetQueryParam(nameof(accountId), accountId)
+                .SetQueryParam(nameof(page), page)
                 .SetQueryParam(nameof(pageSize), pageSize);
 
-            return await request.GetJsonAsync<PagedList<TransactionDto>>();
+            return await request.GetJsonAsync<PagedList<TransactionDto>>(cancellationToken);
         }
+
 
         /// <summary>
         /// Distinct userIds (sender or recipient) of all transactions within the last <paramref name="hours"/> hours.
