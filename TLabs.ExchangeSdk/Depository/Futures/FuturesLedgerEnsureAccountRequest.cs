92b760031c335c7e4ccf0464956fac395638702a
NeerdOner <pas25151+tlabs@gmail.com>
2026-08-02 18:51:54 +0000

    remove futures ledger client, bump 1.0.888

--- TLabs.ExchangeSdk/Depository/Futures/FuturesLedgerEnsureAccountRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace TLabs.ExchangeSdk.Depository.Futures
-{
-    public class FuturesLedgerEnsureAccountRequest
-    {
-        public long FuturesAccountId { get; set; }
-        public string UserId { get; set; }
-        public string CurrencyCode { get; set; }
-    }
-}
--- TLabs.ExchangeSdk/Depository/Futures/FuturesLedgerOperationResult.cs
+++
@@ -1,8 +0,0 @@
-namespace TLabs.ExchangeSdk.Depository.Futures
-{
-    public class FuturesLedgerOperationResult
-    {
-        public FuturesLedgerAccountSnapshot PrimaryBalance { get; set; }
-        public FuturesLedgerAccountSnapshot CounterpartyBalance { get; set; }
-    }
-}
