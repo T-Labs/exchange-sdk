473c74102ce14b19679cc0065f35c1a317676e07
DONIL <danilnorm@gmail.com>
2026-08-03 22:56:22 +0500

    after review

--- TLabs.ExchangeSdk/PaymentCards/Dtos/ActivatePhysicalPaymentCardDto.cs
+++ TLabs.ExchangeSdk/PaymentCards/Dtos/ActivatePhysicalPaymentCardDto.cs
@@ -5,4 +5,7 @@ public class ActivatePhysicalPaymentCardDto
     public string UserId { get; set; }
     public string PrintedCardNumber { get; set; }
     public string Pin { get; set; }
+
+    public override string ToString() =>
+        $"{nameof(ActivatePhysicalPaymentCardDto)}(userId:{UserId}, printedCardNumber:***)";
 }
