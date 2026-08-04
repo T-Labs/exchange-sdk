e76b3cefc2df39fd750e2ca730088d0ee099ad84
DONIL <danilnorm@gmail.com>
2026-08-04 11:32:41 +0500

    deslop

--- TLabs.ExchangeSdk/PaymentCards/Dtos/PaymentCardPhysicalOrderDto.cs
+++ TLabs.ExchangeSdk/PaymentCards/Dtos/PaymentCardPhysicalOrderDto.cs
@@ -6,50 +6,27 @@ namespace TLabs.ExchangeSdk.PaymentCards.Dtos;
 public class PaymentCardPhysicalOrderDto
 {
     public Guid Id { get; set; }
-
     public Guid UserPaymentCardId { get; set; }
-
     public string UserId { get; set; }
-
     public int TemplateId { get; set; }
-
     public Guid? PaymentCardProductId { get; set; }
-
     public PaymentCardPhysicalOrderStatus Status { get; set; }
-
     public PaymentCardStatus? CardStatus { get; set; }
-
     public string MaskedPan { get; set; }
-
     public bool HasPrintedCardNumber { get; set; }
-
     public bool HasActivationPin { get; set; }
-
     public string RegisterEmail { get; set; }
-
     public string RegisterDialCode { get; set; }
-
     public string RegisterPhoneNumber { get; set; }
-
     public string TrackingNumber { get; set; }
-
     public string RecipientName { get; set; }
-
     public string CountryCode { get; set; }
-
     public string Region { get; set; }
-
     public string City { get; set; }
-
     public string Postcode { get; set; }
-
     public string AddressLine { get; set; }
-
     public string AdminNotes { get; set; }
-
     public DateTimeOffset DateCreated { get; set; }
-
     public DateTimeOffset DateUpdated { get; set; }
-
     public DateTimeOffset? DateShipped { get; set; }
 }
--- TLabs.ExchangeSdk/PaymentCards/Dtos/UpdatePaymentCardPhysicalOrderDto.cs
+++ TLabs.ExchangeSdk/PaymentCards/Dtos/UpdatePaymentCardPhysicalOrderDto.cs
@@ -5,20 +5,12 @@ namespace TLabs.ExchangeSdk.PaymentCards.Dtos;
 public class UpdatePaymentCardPhysicalOrderDto
 {
     public PaymentCardPhysicalOrderStatus? Status { get; set; }
-
     public string TrackingNumber { get; set; }
-
     public string RecipientName { get; set; }
-
     public string CountryCode { get; set; }
-
     public string Region { get; set; }
-
     public string City { get; set; }
-
     public string Postcode { get; set; }
-
     public string AddressLine { get; set; }
-
     public string AdminNotes { get; set; }
 }
