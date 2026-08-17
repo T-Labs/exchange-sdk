583a88fa0023c161902408f49d5b2ab929ebde5e
elizarov <iamvizar@mail.ru>
2026-08-17 19:51:45 +0300

    conflict fix

--- TLabs.ExchangeSdk/Depository/ClientDepository.cs
+++ TLabs.ExchangeSdk/Depository/ClientDepository.cs
@@ -105,7 +105,7 @@ namespace TLabs.ExchangeSdk.Depository
             GetTransactionsByAccountIdPaged(accountId, page, pageSize, null, default);
 
         public virtual async Task<PagedList<TransactionDto>> GetTransactionsByAccountIdPaged(Guid accountId,
-            int page = 1, int pageSize = 50, string search = null, CancellationToken cancellationToken = default)
+            int page, int pageSize, string search = null, CancellationToken cancellationToken = default)
         {
             var request = "depository/transaction/by-account/paged".InternalApi()
                 .SetQueryParam(nameof(accountId), accountId)
