bee77567c680885b82882e7f9d3f4de69a0b5d75
DONIL <danilnorm@gmail.com>
2026-09-03 16:19:36 +0500

    fix funds balance

--- TLabs.ExchangeSdk/PaymentCards/Dtos/PaymentCardMerchantRechargeContextDto.cs
+++ TLabs.ExchangeSdk/PaymentCards/Dtos/PaymentCardMerchantRechargeContextDto.cs
@@ -6,7 +6,7 @@ public class PaymentCardMerchantRechargeContextDto
 {
     public decimal MerchantBalance { get; set; }
 
-    public decimal? FundsBalance { get; set; }
+    public decimal FundsBalance { get; set; }
 
     public string TokenSymbol { get; set; }
 
